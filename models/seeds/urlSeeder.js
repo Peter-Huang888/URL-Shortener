3f181ec6972f1f795a12bd106d7c77b783e724c7
Peter-Haung888 <ming31437@gmail.com>
2023-06-23 03:20:08 +0800

    refactor: modify urlSeeder.js

--- models/seeds/urlSeeder.js
+++ models/seeds/urlSeeder.js
@@ -1,12 +1,13 @@
 const db = require('../../config/mongoose')
 const URL = require('../url')
 const urls = ['https://www.google.com', 'https://tw.yahoo.com', 'https://www.facebook.com']
-const shorten = ['wgQa8', 'sMWeu', 'BY5TM']
 
 db.once('open', () => {
   console.log('Add seeds data')
   for (let i = 0; i < 3; i++) {
-    URL.create({ url: `${urls[i]}`, shorten: `${shorten[i]}` })
+    const shortener = require('../../public/javascripts/shortener')
+    const shorten = shortener()
+    URL.create({ url: `${urls[i]}`, shorten: `${shorten}` })
   }
   console.log('done')
 })
