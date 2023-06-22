bfb5aadb00f2f92dc79b5df2b3cffdf3ce61b5a0
Peter-Haung888 <ming31437@gmail.com>
2023-06-23 02:31:18 +0800

    refactor: Refactor shortener.js

--- shortener.js
+++ shortener.js
@@ -1,8 +1,8 @@
-const number = "0123456789"
-const lowerCase = "abcdefghijklmnopqrstuvwxyz"
-const upperCase = lowerCase.toUpperCase()
-const collection = [...number].concat([...lowerCase],[...upperCase])
 function shortener () {
+  const number = "0123456789"
+  const lowerCase = "abcdefghijklmnopqrstuvwxyz"
+  const upperCase = lowerCase.toUpperCase()
+  const collection = [...number].concat([...lowerCase], [...upperCase])
   let shorten = ''
   for (let i = 0; i < 5; i++) {
     const random = Math.floor(Math.random() * collection.length)
