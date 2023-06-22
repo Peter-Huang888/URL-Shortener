5f3b17cb2c52890a4bf109ebf615ae99f3bae447
Peter-Haung888 <ming31437@gmail.com>
2023-06-23 02:56:38 +0800

    refactor: modify Express routes

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const router = require('./routes')
 
 // Include Handlebars
 const exphbs = require('express-handlebars')
@@ -22,48 +23,13 @@ db.on('error', () => {
 db.once('open', () => {
   console.log('mongoDB connected!')
 })
-const URL = require('./models/url')
+
 
 app.use(express.urlencoded({ extended: true }))
 
 app.use(express.static('public'))
 
-app.get('/', (req, res) => {
-  res.render('index')
-})
-
-app.post('/result', (req, res) => {
-  const url = req.body.url.trim('')
-  // if the user does not enter something, a prompt is given
-  if (!url) {
-    const tr = true 
-    return res.render('index', { tr })
-  }
-  return URL.find()
-    .lean()
-    .then(webs => {
-      const filtered = webs.filter(web => web.url === url)
-      // If the same URL is entered, the same shorten address is generated
-      if (filtered.length) {
-        const shorten = filtered[0].shorten
-        return res.render('result', { shorten })
-      // If the different URL is entered, create a new shorten address
-      } else {
-        const shortener = require('./shortener')
-        const shorten = shortener()
-        URL.create({ url: url, shorten: shorten })
-        return res.render('result', { shorten })
-      }
-    })
-    .catch(err => console.log(err))
-})
-
-app.get('/:shortener', (req, res) => {
-  const shortener = req.params.shortener
-  return URL.findOne({ shorten: shortener })
-    .lean()
-    .then(web => res.redirect(`${web.url}`))
-})
+app.use(router)
 
 app.listen(port, () => {
   console.log(`URL shortener is running on http://localhost:${port}`)
---
+++ routes/index.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const router = express.Router()
+const URL = require('../models/url')
+
+router.get('/', (req, res) => {
+  res.render('index')
+})
+router.post('/result', (req, res) => {
+  const url = req.body.url.trim('')
+  // if the user does not enter something, a prompt is given
+  if (!url) {
+    const tr = true
+    return res.render('index', { tr })
+  }
+  return URL.find()
+    .lean()
+    .then(webs => {
+      const filtered = webs.filter(web => web.url === url)
+      // If the same URL is entered, the same shorten address is generated
+      if (filtered.length) {
+        const shorten = filtered[0].shorten
+        return res.render('result', { shorten })
+        // If the different URL is entered, create a new shorten address
+      } else {
+        const shortener = require('./shortener')
+        const shorten = shortener()
+        URL.create({ url: url, shorten: shorten })
+        return res.render('result', { shorten })
+      }
+    })
+    .catch(err => console.log(err))
+})
+
+router.get('/:shortener', (req, res) => {
+  const shortener = req.params.shortener
+  // 透過shortener從資料庫撈資料後，再導向原本的url
+  return URL.findOne({ shorten: shortener })
+    .lean()
+    .then(web => res.redirect(`${web.url}`))
+})
+
+module.exports = router
