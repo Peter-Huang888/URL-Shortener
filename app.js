0f0b21fe2cefc89e2e2f57ce84c1afd23425f26a
Peter-Haung888 <ming31437@gmail.com>
2023-06-22 19:41:26 +0800

    feat: Add view engine Handlebars

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
+// Include Express
 const express = require('express')
 const app = express()
 const port = 3000
 
+// Include Handlebars
+const exphbs = require('express-handlebars')
+app.engine('.hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
 app.get('/', (req, res) => {
-  res.send(`This is an url shortener website.`)
+  res.render('index')
+})
+
+app.post('/result', (req, res) => {
+  res.render('result')
 })
 
 app.listen(port, () => {
-  console.log(`Express is running on http://localhost:${port}`)
+  console.log(`URL shortener is running on http://localhost:${port}`)
 })
